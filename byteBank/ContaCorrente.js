84bbbfdfac54530e225760f684e20e0d8090a632
Jorge Nascimento <jorg3nascimento@gmail.com>
2020-07-23 15:07:45 -0300

    continuando projeto byteBank

--- byteBank/ContaCorrente.js
+++ byteBank/ContaCorrente.js
@@ -1,15 +1,14 @@
-import{Cliente} from "./Cliente.js"
+import{ Cliente } from "./Cliente.js"
 
 export class ContaCorrente {
     static numeroDeConta = 0;
-    agencia = 1001;
-    _cliente = new Cliente;
+   
 
-    _saldo = 0;
-
-    constructor( agencia, cliente){
+    constructor(agencia, cliente){
         this.agencia = agencia;
         this.cliente = cliente;
+        this._saldo = 0;
+
         ContaCorrente.numeroDeConta += 1;
     }
 
@@ -26,30 +25,5 @@ export class ContaCorrente {
     get saldo(){
         return this._saldo;
     }
-    sacar(valor) {
-        if ( valor <= 0 || this._saldo < valor) {
-
-            console.log("Saldo Insuficiente, o valor em conta é de R$" + this._saldo)
-
-            return;
-            
-        }
-        this._saldo -= valor;
-        console.log("Valor sacado R$"+valor)
-        console.log("Saldo atual R$"+ this._saldo)
-    }
-
-    depositar(valor){
-        if(valor <= 0) return;
-
-        this._saldo += valor;
-        console.log("Foi depositado o valor R$ " + valor)       
-        
-    }
-
-    transferir(valor, conta){
-        const valorTranferido = this.sacar(valor);
-
-        conta.depositar(valor);
-    }
+    
 }
--- byteBank/index.js
+++ byteBank/index.js
@@ -1,32 +1,12 @@
-import {Cliente} from "./Cliente.js"
-import {ContaCorrente} from "./ContaCorrente.js"
+import { Cliente } from "./Cliente.js";
+import { Conta } from "./Conta.js"
+import { ContaCorrente } from "./ContaCorrente.js";
+import { ContaPoupanca } from "./ContaPoupanca.js";
 
 // Criando classes, cria-se moldes para instancia de objetos no sistema, nas classes se define atributos (características) e métodos ( comportamentos). 
 
-const cliente1 = new Cliente();
+const cliente1 = new Cliente("Jorge", "533.544.584.78")
 
-cliente1.nome = "Jorge"
-cliente1.cpf = "386.555.878-56"
+console.log(cliente1)
 
-const cliente2 = new Cliente();
-
-cliente2.nome = "Ana"
-cliente2.cpf = "333.444.555.87"
-
-
-const clienteJorge = new ContaCorrente();
-clienteJorge.cliente = cliente1;
-
-const conta2 = new ContaCorrente();
-
-conta2.cliente = 0;
-
-
-clienteJorge.depositar(500);
-
-console.log(clienteJorge);
-
-clienteJorge.transferir(200, conta2)
-
-console.log(clienteJorge, conta2);
 
