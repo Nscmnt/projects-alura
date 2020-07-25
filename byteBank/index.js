352f2575e3f0f832ae81431c967021306f915785
Jorge Nascimento <jorg3nascimento@gmail.com>
2020-07-25 13:35:46 -0300

    Finalizando curso de Orientação a Objetos

--- byteBank/index.js
+++ byteBank/index.js
@@ -1,12 +1,16 @@
-import { Cliente } from "./Cliente.js";
-import { ContaCorrente } from "./ContaCorrente.js";
-import { ContaPoupanca } from "./ContaPoupanca.js";
-
+import {Cliente} from "./Cliente.js"
+import { Gerente } from "./Funcionario/Gerente.js"
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js"
  
+const gerente = new Gerente("Jorge", 8000, 1234568799)
 
-const cliente1 = new Cliente("Jorge", "11111555554444");
+gerente.cadastrarSenha("12345");
 
-const contaJorge = new ContaCorrente(cliente1, 10001);
+const cliente =new Cliente("Jorge", 25452365874, "123")
+
+const clienteLogado = SistemaAutenticacao.login(cliente, "123")
+
+console.log(clienteLogado)
 
 
 
